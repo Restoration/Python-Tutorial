04a91a1ef867334f221386104fe7d69f4d936ef3
RyotArch <r.yamamoto.system@gmail.com>
2019-11-03 05:50:04 +0900

    basic types

--- src/variables-data-types/app.py
+++ src/variables-data-types/app.py
@@ -1,5 +1,6 @@
 character_name = "John"
-character_age = "35"
+character_age = 50
+is_male = True
 print("There once was a man named " + character_name + ", ")
 print("he was  "+ character_age +" years old.")
 
