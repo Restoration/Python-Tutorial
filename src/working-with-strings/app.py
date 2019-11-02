16b77f2c3593a3ff2e68b70814b671925081ccdd
RyotArch <r.yamamoto.system@gmail.com>
2019-11-03 05:59:22 +0900

    basic string

---
+++ src/working-with-strings/app.py
@@ -0,0 +1,23 @@
+print("Giraffe \nAcademy")
+print("Giraffe \"Academy")
+print("Giraffe \Academy")
+
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+
+print(phrase.lower())
+print(phrase.upper())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+
+print(phrase[0])
+
+print(phrase.index("G"))
+print(phrase.index("Acad"))
+# print(phrase.index("z")) # error
+
+print(phrase.replace("Giraffe", "Elephant"))
+
+
