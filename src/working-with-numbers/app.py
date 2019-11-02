e46d200b2f155e0a259741b82b68fe72cc1831fc
RyotArch <r.yamamoto.system@gmail.com>
2019-11-03 08:18:56 +0900

    basic number

---
+++ src/working-with-numbers/app.py
@@ -0,0 +1,26 @@
+print(2)
+print(2.0987)
+print(-2.0987)
+print( 3 * (4 + 5 ))
+print( 10 % 3)
+
+my_num = 5
+print(my_num)
+
+
+print(str(my_num) + " my favorite number")
+
+
+
+n_my_num = -5
+print(abs(n_my_num))
+
+print(pow(4, 6))
+
+
+print(max(4, 6))
+print(min(4, 6))
+
+print(round(3.7))
+
+
---
+++ src/working-with-numbers/math.py
@@ -0,0 +1,7 @@
+from math import *
+
+my_num = -5
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
+
