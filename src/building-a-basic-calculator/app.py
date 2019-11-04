ca346124b08313e7b32fb89c60a450a519121c06
RyotArch <r.yamamoto.system@gmail.com>
2019-11-04 22:21:48 +0900

    building a basic calculator

---
+++ src/building-a-basic-calculator/app.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number: ");
+num2 = input("Enter another number: ");
+# result = num1 + num2 # here is string
+#result = int(num1) + int(num2) # only decimal number
+
+result = float(num1) + float(num2)
+
+print(result)
