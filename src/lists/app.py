b78114c17cd9d897c03b041a65c472a32b2ef15d
RyotArch <r.yamamoto.system@gmail.com>
2019-11-05 23:51:54 +0900

    Lists

---
+++ src/lists/app.py
@@ -0,0 +1,14 @@
+friends = ["Kevin", "Karen", "Jim"]
+
+print(friends[0]) # Kevin
+print(friends[-1]) # Jim
+print(friends[1:]) # ['Karen', 'Jim']
+
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+
+print(friends[1:3]) # ['Karen', 'Jim']
+
+friends[1] = "Mike"
+
+print(friends[1]) # Mike
