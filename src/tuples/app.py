4cb9559142ec234e66743f005a3b072574ee5910
RyotArch <r.yamamoto.system@gmail.com>
2019-11-11 09:25:48 +0900

    tuples

---
+++ src/tuples/app.py
@@ -0,0 +1,8 @@
+# Tuples is Immutable
+# tuples syntax defferents list
+coordinates = (4, 5)
+# coordinates[0] = 10 # not working
+print(coordinates[1])
+
+# Also you can use tuples inside list
+coordinates2 = [(4, 5), (6, 7), (80, 34)]
