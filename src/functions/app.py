dbed7e6fa07204c5a9758feb3e0ddbced1fbb58f
RyotArch <r.yamamoto.system@gmail.com>
2019-11-11 09:32:50 +0900

    Functions

---
+++ src/functions/app.py
@@ -0,0 +1,12 @@
+def sayhi():
+    print("Hello User")
+
+print("Top")
+sayhi()
+print("Bottom")
+
+def say_hi(name, age):
+    print("Hello " + name + ", You are " + str(age))
+
+say_hi("Mike", 35);
+say_hi("Steve", 70);
